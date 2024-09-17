377eddff14eddadb3c0f26453807bd307ea30b18
viniciussiegle <viniciussiegle@gmail.com>
2024-09-17 16:37:25 +0200

    Add support for prompting from available stocks

--- src/main/java/realtime/Main.java
+++ src/main/java/realtime/Main.java
@@ -1,6 +1,7 @@
 package realtime;
 
 import java.sql.*;
+import java.util.*;
 
 
 public class Main {
@@ -13,34 +14,70 @@ public class Main {
         // save data to new database
 
 
-        // *retrieve data from the database (initially, only average opening prices)
-        String stock = "googl";
-        String query = "select avg(Open) from " + stock;
-        String url = "jdbc:sqlite:data/stocks.db";
+        // retrieve data from the database (initially, only average opening prices)
+        float avg = getAverageOpening();
 
+        // calculate amd print analysis values
+        if (avg == 0) {
+            System.out.println("No data found");
+        }
+        else {
+            System.out.println("Average opening price is: " + avg);
+        }
+
+        // (update data and analysis)
+
+
+    }
+
+    /**
+     * Gets the average opening price of a stock from all available ones in the database for a prompted stock ticker.
+     * @return the average opening price
+     */
+    private static float getAverageOpening() {
+        String preparedQuery = "select avg(Open) from ";
+        String url = "jdbc:sqlite:data/stocks.db";
         float avg = 0;
         try (
                 Connection connection = DriverManager.getConnection(url);
                 Statement statement = connection.createStatement()
         ) {
-            ResultSet resultSet = statement.executeQuery(query);
+            String stock = getStockTicker(connection);
+
+            ResultSet resultSet = statement.executeQuery(preparedQuery + stock);
             resultSet.next();
             avg = resultSet.getFloat(1);
         }
         catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return avg;
+    }
 
-        // *calculate amd print analysis values
-        if (avg == 0) {
-            System.out.println("No data found");
+    /**
+     * Prompts the user for a stock ticker. Only allows tickers existing in the database to avoid injections.
+     * @param connection the connection to the database
+     * @return the stock ticker
+     * @throws SQLException if a database access error occurs
+     */
+    private static String getStockTicker(Connection connection) throws SQLException {
+        // Get available stock tickers based on existing table names
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", null);
+        List<String> availableStocks = new ArrayList<>();
+        tables.next();
+        while (tables.next()) {
+            availableStocks.add(tables.getString("TABLE_NAME"));
         }
-        else {
-            System.out.println("Average opening price for '" + stock + "' is: " + avg);
-        }
 
-        // (update data and analysis)
+        // Prompt user for stock ticker, restricting to existing ones to avoid injections
+        Scanner scanner = new Scanner(System.in);
+        String stock;
+        do {
+            System.out.println("Please enter stock ticker (" + availableStocks + "): ");
+            stock = scanner.nextLine();
+        } while (!availableStocks.contains(stock));
 
-
+        return stock;
     }
 }
